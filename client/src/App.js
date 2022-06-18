8def50bbaccfddbe4a0a559f6d7421108a22b012
Marissa Halim <mhalim@hawaii.edu>
2022-06-18 22:31:44 +0700

    updated navbar and added cart icon button

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React  from 'react';
 import './App.css';
 import Home from './pages/Home';
 import Shop from './pages/Shop';
+import Cart from './pages/Cart';
 import Signup from './components/user/Signup';
 import Signin from './components/user/Signin';
 import CreateSticker from './pages/CreateSticker';
@@ -17,6 +18,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home />}/>
           <Route exact path='/Shop' element={<Shop />}/>
+          <Route exact path='/Cart' element={<Cart />}/>
           <Route exact path='/CreateSticker' element={<CreateSticker />}/>
           <Route exact path='/Signup' element={<Signup />}></Route>
           <Route exact path='/Signin' element={<Signin />}></Route>
---
+++ client/src/pages/Cart.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+function Cart() {
+
+  return (
+    <Container className="p-5 text-center">
+      <h1> Your Sticker Cart </h1>
+    </Container>
+  );
+}
+
+export default Cart;
